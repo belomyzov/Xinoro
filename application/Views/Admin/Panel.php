bb37af218b6f07160f195240e3499ca4ff86891a
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-11-25 18:30:33 +0300

    Update css style

--- application/Views/Admin/Panel.php
+++ application/Views/Admin/Panel.php
@@ -2,40 +2,21 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Авторизация в админ панель</title>
+		<title>Панель администратора</title>
 	</head>
 	<body>
 
-		<svg  xmlns="http://www.w3.org/2000/svg" >
-		    <circle cx="19px" cy="300px" r="300px" fill="#6393DC" />
-		</svg>
-
-		<svg  xmlns="http://www.w3.org/2000/svg">
-		    <circle cx="200px" cy="420px" r="300px" fill="#699BE5" />
-		</svg>
-
-		<div class="parent">
-		    <div class="block">
-		        <img align="center" src="../vendor/image/logo.png" alt=""/>
-				<form action="login" method="POST">
-					<p><input class="text-field__input" type="text" name="login" required placeholder="Логин"></p>
-					<p><input class="text-field__input" type="password" name="password" required placeholder="Пароль"></p>
-					<?php
-						if(isset($_POST["aloginbtn"]))
-						{
-							if($_POST['login'] != "root" && $_POST['password'] != "root")
-							{
-								echo "<div class='warning'><p> Access denied </p></div>";
-							}
-							else
-							{
-								return header("Location: panel");
-							}
-						}
-					?>
-					<input class="btn" type="submit" name="aloginbtn" value="Войти"/>
-				</form>
-		    </div>
+		<div class="left-menu">
+			<a href="../"><img src="vendor/image/pushka.png"></a>
+		</div>
+		<div class="them-box">
+			Тест
+		</div>
+		<div class="task-box">
+			Тест
+		</div>
+		<div class="user-img-box">
+			Тест
 		</div>
 
 		<style type="text/css"> 
