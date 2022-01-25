fd5c8552108f320988191c3d4370fa988e225802
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-01-25 23:58:21 +0300

    change page show

--- core/views/admin/panel.php
+++ core/views/admin/panel.php
@@ -5,6 +5,7 @@
 	<body>
 		<header>
 			<a href="admin/panel" class="logo"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/logob.png?raw=true" alt=""></a>
+			<!-- Menu start -->
 			<div class="nav">
 				<ul>
 					<li><a href="admin/panel"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/Main-Icon.png?raw=true" alt=""></a></li>
@@ -13,36 +14,45 @@
 					<li><a href="admin/setting"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/Main-Setting-Icon.png?raw=true" alt=""></a></li>
 				</ul>
 			</div>
+			<!-- Menu end -->
 		</header>
 		<content>
 			<div class="info-box">
+				<!-- Edit panel start -->
 				<div class="buttons">
 					<div class="label">Pages</div>
 					<div onclick="openPageControll();" class="addpages">добавить</div>
 				</div>
+				<!-- Edit panel stop -->
+				<!-- Вывод всех страниц -->
 				<div class="pages">
-					<?php 
+					<?php
 						$pages = json_decode(file_get_contents("config/routes.json"),true);
 						foreach($pages['routes'] as $value)
 						{
+							# Передача переменных из routes.json 
+							# URL - 0 (string)
+							# Controller - 1 (string)
+							# Action - 2 (string)
+							# Title - 3 (string) 
+
+							# openInEditor() - javascript(func) - Открытие страницы в редакторе
+
 							$editKey = "'".$value[1]."','".$value[2]."'";
-							echo '
-								<div onclick="openInEditor('.$editKey.');" class="page">
-									<div class="url">'.$value[0].'</div>
-									<div class="controller">'.$value[1].'</div>
-									<div class="action">'.$value[2].'</div>
-								</div>
-							';
+							echo '<div onclick="openInEditor('.$editKey.');" class="page">
+									<div class="url">'.$value[3].'</div></div>';
 						}
 					?>
 				</div>
 			</div>
-			<div id="create-page-controll" class="box-add">
-				<div class="label-func">Создание страницы</div>
-				<input type="text" id="url" placeholder="Url">
-				<input type="text" id="controller" placeholder="Controller">
-				<input type="text" id="action" placeholder="Action">
-				<div onclick="createPage();" class="create-page-btn">Создать</div>
+			<div class="modal-box">
+				<div id="create-page-controll" class="box-add">
+					<div class="label-func">Создание страницы</div>
+					<input type="text" id="url" placeholder="Url">
+					<input type="text" id="controller" placeholder="Controller">
+					<input type="text" id="action" placeholder="Action">
+					<div onclick="createPage();" class="create-page-btn">Создать</div>
+				</div>
 			</div>
 		</content>
 	</body>
